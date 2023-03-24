b89ebccaad8900dced3d9af05b0ed0d5da144cc3
HenriqueMonteiro <Henriquepupet@gmail.com>
2023-03-24 10:38:43 +0000

    Added PowerOf2Until15()

--- Power2Method/Program.cs
+++ Power2Method/Program.cs
@@ -9,12 +9,21 @@ namespace Power2Method
             for(int i = 1; i <= ( 1 << 5 ); i = i << 1 )
             {
                 Console.WriteLine(i);
+            }   
+        }
+        private static void PowersOfUntilN(int n)
+        {
+            for(int i = 1; i <= ( 1 << n ); i = i << 1 )
+            {
+                Console.WriteLine(i);
             }
         }
         private static void Main()
         {
             PowersOf2Until5();
             PowersOf2Until5();
+            PowersOfUntilN(12);
+            PowersOfUntilN(15);
 
         }
    }
